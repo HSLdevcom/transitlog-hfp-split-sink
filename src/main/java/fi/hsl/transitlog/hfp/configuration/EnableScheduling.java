a6f03ac617ebadca085209563db149c9902611ba
Aki Väliaho <aki.valiaho@hsl.fi>
2019-11-06 09:36:35 +0200

    Tune scheduler threadpool

--- src/main/java/fi/hsl/transitlog/hfp/configuration/EnableScheduling.java
+++ src/main/java/fi/hsl/transitlog/hfp/configuration/EnableScheduling.java
@@ -1,8 +1,19 @@
 package fi.hsl.transitlog.hfp.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @org.springframework.scheduling.annotation.EnableScheduling
 @Configuration
 public class EnableScheduling {
+    @Bean
+    public TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+        threadPoolTaskScheduler.setPoolSize(4);
+        threadPoolTaskScheduler.initialize();
+        return threadPoolTaskScheduler;
+    }
+
 }
