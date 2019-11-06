a97b24aaf436bef0ec3968c2e9ac973c9306b7bd
Joose Helle <joose.helle@gmail.com>
2019-11-06 11:53:31 +0200

    Fix concurrentmodificationexception

--- src/main/java/fi/hsl/transitlog/hfp/persisthfpdata/DomainMappingWriter.java
+++ src/main/java/fi/hsl/transitlog/hfp/persisthfpdata/DomainMappingWriter.java
@@ -14,6 +14,7 @@ import javax.persistence.EntityManager;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ScheduledExecutorService;
 
 @Slf4j
@@ -29,7 +30,7 @@ public class DomainMappingWriter {
 
     @Autowired
     DomainMappingWriter(PulsarApplication pulsarApplication, EntityManager entityManager, DumpService dumpTask) {
-        eventQueue = new HashMap<>();
+        eventQueue = new ConcurrentHashMap<>();
         this.dumpTask = dumpTask;
         this.entityManager = entityManager;
         this.pulsarApplication = pulsarApplication;
