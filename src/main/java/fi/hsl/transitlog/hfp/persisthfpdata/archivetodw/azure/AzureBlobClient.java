528299223f1e13373e0f9cf8ea7bc1383c55b6b6
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2022-05-30 16:07:11 +0300

    Create Azure blob container client lazily to avoid creating empty containers if CSV upload is disabled

--- src/main/java/fi/hsl/transitlog/hfp/persisthfpdata/archivetodw/azure/AzureBlobClient.java
+++ src/main/java/fi/hsl/transitlog/hfp/persisthfpdata/archivetodw/azure/AzureBlobClient.java
@@ -6,28 +6,39 @@ import org.springframework.stereotype.*;
 
 @Component
 public class AzureBlobClient {
-    BlobContainerClient blobContainerClient;
+    private final BlobServiceClient blobServiceClient;
+    private final String blobContainer;
+
+    private BlobContainerClient blobContainerClient;
 
     AzureBlobClient(@Value(value = "${blobstorage.connectionString}") String connectionString, @Value(value = "${blobstorage.blobcontainer}") String blobContainer) {
-        BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
+        this.blobServiceClient = new BlobServiceClientBuilder()
                 .connectionString(connectionString)
                 .buildClient();
-        if (blobServiceClient.getBlobContainerClient(blobContainer).exists())
-            this.blobContainerClient = blobServiceClient.getBlobContainerClient(blobContainer);
-        else
-            this.blobContainerClient = blobServiceClient.createBlobContainer(blobContainer);
 
+        this.blobContainer = blobContainer;
+    }
+
+    public synchronized BlobContainerClient getBlobContainerClient() {
+        if (blobContainerClient == null) {
+            if (blobServiceClient.getBlobContainerClient(blobContainer).exists()) {
+                this.blobContainerClient = blobServiceClient.getBlobContainerClient(blobContainer);
+            } else {
+                this.blobContainerClient = blobServiceClient.createBlobContainer(blobContainer);
+            }
+        }
+
+        return blobContainerClient;
     }
 
     void uploadFromFile(String filePath) {
-        BlobClient blobClient = blobContainerClient.getBlobClient(filePath);
+        BlobClient blobClient = getBlobContainerClient().getBlobClient(filePath);
         blobClient.uploadFromFile(filePath, true);
-
     }
 
     public boolean fileExists(String filePath) {
-        blobContainerClient.getBlobClient(filePath);
-        return blobContainerClient.exists();
+        getBlobContainerClient().getBlobClient(filePath);
+        return getBlobContainerClient().exists();
     }
 }
 
