477bc41142acd78997a5e3604e55c30cead74d53
Anton Jyrkiäinen <anton.jyrkiainen@helsinki.fi>
2023-10-12 17:56:29 +0300

    env updates

--- start-application.sh
+++ start-application.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -Xms256m -Xmx4096m -Dtimescale_db_username=$(cat /run/secrets/TRANSITLOG_TIMESCALE_USERNAME) -Dtimescale_db_password=$(cat /run/secrets/TRANSITLOG_TIMESCALE_PASSWORD) -jar /usr/app/transitlog-hfp-split-sink.jar
+java -Xms256m -Xmx4096m -Dtimescale_db_username=$TRANSITLOG_TIMESCALE_USERNAME -Dtimescale_db_password=$TRANSITLOG_TIMESCALE_PASSWORD -jar /usr/app/transitlog-hfp-split-sink.jar
